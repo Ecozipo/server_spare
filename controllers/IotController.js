43420bd3164cb3d68cae66434bb372976334f798
= <rantsajonathan@gmail.com>
2024-09-20 16:16:44 +0300

    fix database url

--- controllers/IotController.js
+++ controllers/IotController.js
@@ -1,73 +1,80 @@
 import device from "../utils/awsDevice.js";
 import { io } from "socket.io-client";
 import { getPower, setPower, setId } from "../data/State.js";
+import jwt from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
 // import { redisClient } from "../utils/redis.js";
 // import { createClient } from "redis";
 
-const socket = io('ws://localhost:5000')
+const socket = io("ws://localhost:5000");
+const prisma = new PrismaClient();
 // let redisClient = createClient();
 
 export const publishCommand = async (req, res) => {
-    const state = req.body;
-    // device.publish('esp32/led', JSON.stringify(message), (err) => {
-    //     if (err) {
-    //         console.error('Error publishing message:', err);
-    //         res.status(500).send('Error publishing message');
-    //     } else {
-    //         console.log('Message published successfully', JSON.stringify(message));
-    //         res.status(200).json(message);
-    //     }
-    // });
+  const state = req.body;
+  // device.publish('esp32/led', JSON.stringify(message), (err) => {
+  //     if (err) {
+  //         console.error('Error publishing message:', err);
+  //         res.status(500).send('Error publishing message');
+  //     } else {
+  //         console.log('Message published successfully', JSON.stringify(message));
+  //         res.status(200).json(message);
+  //     }
+  // });
 
-    device.publish('$aws/things/Spare/shadow/update', JSON.stringify(state), (err) => {
-        if (err) {
-            console.error('Error publishing message:', err);
-            res.status(500).send('Error publishing message');
-        } else {
-            console.log('Message published successfully');
-            res.sendStatus(200);
-        }
-    });
-
+  device.publish(
+    "$aws/things/Spare/shadow/update",
+    JSON.stringify(state),
+    (err) => {
+      if (err) {
+        console.error("Error publishing message:", err);
+        res.status(500).send("Error publishing message");
+      } else {
+        console.log("Message published successfully");
+        res.sendStatus(200);
+      }
+    }
+  );
 };
 
 export const subscribeData = async (req, res) => {
-    const token = req.headers.authorization.split(" ")[1];
+  const token = req.headers.authorization.split(" ")[1];
 
-    const { id } = jwt.decode(token).utilisateur
+  const { id } = jwt.decode(token).utilisateur;
 
-    const utilisateur = await prisma.utilisateur.findUnique({ where: { id: parseInt(id) } })
+  const utilisateur = await prisma.utilisateur.findUnique({
+    where: { id: parseInt(id) },
+  });
 
-    if (!utilisateur) {
-        res.status(500).send({ errorMessage: "Utilisateur introuvable" })
+  if (!utilisateur) {
+    res.status(500).send({ errorMessage: "Utilisateur introuvable" });
+  }
+  setId(parseInt(id));
+
+  console.log(getPower());
+  let lastValue = getPower();
+
+  socket.on("send_stats", (data) => {
+    console.log("listening to data");
+    if (data.power) {
+      setPower(parseInt(data.power) + lastValue);
     }
-    setId(parseInt(id))
-
-    console.log(getPower())
-    let lastValue = getPower()
-
-    socket.on("send_stats", (data) => {
-        console.log("listening to data")
-        if (data.power) {
-            setPower(parseInt(data.power) + lastValue)
-        }
-        res.status(200).json({ power: getPower() })
-        console.log(data)
-    })
+    res.status(200).json({ power: getPower() });
+    console.log(data);
+  });
 };
 
-
 export const totalPower = async (req, res) => {
-    res.status(200).json({ power: getPower() })
-}
+  res.status(200).json({ power: getPower() });
+};
 
 export const subscribeStateLed = async (req, res) => {
-    let somme = 0;
+  let somme = 0;
 
-    socket.on("state_led", (data) => {
-        // console.log({data})
-        setTimeout(() => {
-            console.log({power: getPower()})
-        }, 1000)
-    })
+  socket.on("state_led", (data) => {
+    // console.log({data})
+    setTimeout(() => {
+      console.log({ power: getPower() });
+    }, 1000);
+  });
 };
