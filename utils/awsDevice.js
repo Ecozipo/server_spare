2ae7340b058c5498831c87f26ff32117783e5ecc
Jossuc4 <larryjossuc4@gmail.com>
2024-09-25 14:39:33 +0300

    tekeco

--- utils/awsDevice.js
+++ utils/awsDevice.js
@@ -4,7 +4,7 @@ const device = awsIot.device({
   keyPath: "config/credentials/81e5e6a3a666ef15c189137f854e262068c25f1af24258a82c73a56e15ce7800-private.pem.key",
   certPath: 'config/credentials/81e5e6a3a666ef15c189137f854e262068c25f1af24258a82c73a56e15ce7800-certificate.pem.crt',
   caPath: 'config/credentials/AmazonRootCA1.pem',
-  clientId: 'client',
+  clientId: 'client_spare',
   host: 'a27g25yfuax5ui-ats.iot.us-east-1.amazonaws.com'
 });
 
