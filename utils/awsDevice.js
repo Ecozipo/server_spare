842103b5ed5749f8bd71b420fd23f89d4b850dab
= <=>
2024-09-26 16:17:13 +0300

    commit avant test encadreur

--- utils/awsDevice.js
+++ utils/awsDevice.js
@@ -13,7 +13,7 @@ const device = awsIot.device({
     keyPath: "config/credentials/81e5e6a3a666ef15c189137f854e262068c25f1af24258a82c73a56e15ce7800-private.pem.key",
     certPath: 'config/credentials/81e5e6a3a666ef15c189137f854e262068c25f1af24258a82c73a56e15ce7800-certificate.pem.crt',
     caPath: 'config/credentials/AmazonRootCA1.pem',
-    clientId: client_id,
+    clientId: 'spare_client',
     host: 'a27g25yfuax5ui-ats.iot.us-east-1.amazonaws.com'
 });
 
