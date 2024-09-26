9f1c7c6cba3c4d0cee5dbdc699bc7bc1edf30681
= <=>
2024-09-26 16:18:20 +0300

    commit avant test encadreur

--- utils/awsDevice.js
+++ utils/awsDevice.js
@@ -1,13 +1,6 @@
 
 import awsIot from 'aws-iot-device-sdk';
-import io from './socketio.js';
 
-let client_id = "spare_client"
-
-io.on('connection',(socket)=>{
-    client_id = socket.id
-    console.log(`device created successfully : ${client_id}`)
-})
 
 const device = awsIot.device({
     keyPath: "config/credentials/81e5e6a3a666ef15c189137f854e262068c25f1af24258a82c73a56e15ce7800-private.pem.key",
