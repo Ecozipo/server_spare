db82d35c56516179519cf2e9db2ae12ada4ab42b
= <=>
2024-09-26 10:51:39 +0300

    commit avant test encadreur

--- utils/awsDevice.js
+++ utils/awsDevice.js
@@ -2,18 +2,19 @@
 import awsIot from 'aws-iot-device-sdk';
 import io from './socketio.js';
 
-let device = {}
+let client_id = "spare_client"
 
 io.on('connection',(socket)=>{
-    device = awsIot.device({
-        keyPath: "config/credentials/81e5e6a3a666ef15c189137f854e262068c25f1af24258a82c73a56e15ce7800-private.pem.key",
-        certPath: 'config/credentials/81e5e6a3a666ef15c189137f854e262068c25f1af24258a82c73a56e15ce7800-certificate.pem.crt',
-        caPath: 'config/credentials/AmazonRootCA1.pem',
-        clientId: socket.id,
-        host: 'a27g25yfuax5ui-ats.iot.us-east-1.amazonaws.com'
-    });
-
-    console.log('device created successfully')
+    client_id = socket.id
+    console.log(`device created successfully : ${client_id}`)
 })
+
+const device = awsIot.device({
+    keyPath: "config/credentials/81e5e6a3a666ef15c189137f854e262068c25f1af24258a82c73a56e15ce7800-private.pem.key",
+    certPath: 'config/credentials/81e5e6a3a666ef15c189137f854e262068c25f1af24258a82c73a56e15ce7800-certificate.pem.crt',
+    caPath: 'config/credentials/AmazonRootCA1.pem',
+    clientId: client_id,
+    host: 'a27g25yfuax5ui-ats.iot.us-east-1.amazonaws.com'
+});
 
 export default device
