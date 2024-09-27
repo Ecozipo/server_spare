9b7dfe43ae3c5bb94316e6b642460ff1ef6aff50
= <=>
2024-09-27 22:21:52 +0300

    un putain d'erreur de frappe dans tekeco.js

--- tekeco.js
+++ tekeco.js
@@ -194,4 +194,4 @@ app.listen(3000, () => {
 
 cron.schedule('0 * * * *', () => { saveValue(getPower()) })
 cron.schedule('* * * * *', () => { analyses() })
-cron.schedule(`${Math.round(Math.random()*60)} ${Math.rouond(Math.random()*24)} * * *`, () => { getAssistances() })
+cron.schedule(`${Math.round(Math.random()*60)} ${Math.round(Math.random()*24)} * * *`, () => { getAssistances() })
