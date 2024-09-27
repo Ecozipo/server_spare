984c3336a1f8dc52dbd2daeac78eea0494ee13e0
= <=>
2024-09-27 16:37:13 +0300

    automatic saving data finished

--- data/State.js
+++ data/State.js
@@ -38,49 +38,55 @@ export const setData = (id, power) => {
 }
 
 export const saveValue = async (value) => {
-
-    const preview = await prisma.consomation.findMany(
-        {
-            orderBy: {
-                date_consommation: 'desc'
-            },
-            take: 1
-        }
-    )
-
-    console.log(preview)
-
-    preview.forEach(element => {
-        element.valeur = format_data(element.valeur)
-    })
-
-    let p_valeur = preview[0].valeur
-
-    console.log(preview[0], value)
-
-    const actual_value = format_data(JSON.stringify(value))
-
-    const now = moment().tz('Indian/Antananarivo').format('HH:mm:ss')
-    const temps = now.split(":")
-
-    temps.forEach((element, index) => {
-        temps[index] = parseInt(element)
-    })
-
-    try {
-        const creation = await prisma.consomation.create({
-            data: {
-                valeur: JSON.stringify(`{power:${p_valeur.power},energy:${actual_value.energy-preview[0].total}}`),
-                total: actual_value.energy,
-                date_consommation: new Date()
-            }
-        })
-        console.log({ message: "Enregistrement effectué" , data: creation })
-    } catch (error) {
-        console.log({ errorMessage: "Erreur de connexion" })
-        console.log(error)
-    }
+    
     if (temps[0] === 0 && temps[1] === 0 && temps[2] === 0) {
+
+        try {
+
+            const preview = await prisma.consomation.findMany(
+                {
+                    orderBy: {
+                        date_consommation: 'desc'
+                    },
+                    take: 1
+                }
+            )
+        
+            console.log(preview)
+        
+            preview.forEach(element => {
+                element.valeur = format_data(element.valeur)
+            })
+        
+            let p_valeur = preview[0].valeur
+        
+            // console.log(preview[0], value)
+        
+            const actual_value = format_data(JSON.stringify(value))
+        
+            const now = moment().tz('Indian/Antananarivo').format('HH:mm:ss')
+            const temps = now.split(":")
+        
+            temps.forEach((element, index) => {
+                temps[index] = parseInt(element)
+            })
+
+            const creation = await prisma.consomation.create({
+                data: {
+                    valeur: JSON.stringify(`{power:${p_valeur.power},energy:${actual_value.energy-preview[0].total}}`),
+                    total: actual_value.energy,
+                    date_consommation: new Date()
+                }
+            })
+
+            console.log({ message: "Enregistrement effectué" , data: creation })
+
+        } catch (error) {
+
+            console.log({ errorMessage: "Erreur de connexion" })
+            console.log(error)
+            
+        }
     }
 
 }
