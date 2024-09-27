cbe4df3407882879ed52f95f75af01e33c94d2cd
= <=>
2024-09-28 00:45:18 +0300

    j'abandonne pox le fournisseur

--- controllers/FournisseurController.js
+++ controllers/FournisseurController.js
@@ -5,21 +5,7 @@ const prisma = new PrismaClient()
 
 export const getAllFournisseurs = async (req, res) => {
     try {
-        const allFournisseurs = await prisma.fournisseur.findMany({
-            select:{
-                id:true,
-                nom:true,
-                telephone:true,
-                quartier:{
-                    select:{
-                        quartier:true
-                    }
-                },
-                image:true,
-                description:true,
-                url:true
-            }
-        })
+        const allFournisseurs = await prisma.fournisseur.findMany()
 
         res.status(200).send(allFournisseurs)
 
