6c42dc9a1d5fb6036d69fd3861262ee64a1b0a7f
= <=>
2024-09-27 14:16:04 +0300

    commit avant test encadreur

--- controllers/ConsommationController.js
+++ controllers/ConsommationController.js
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
 const format_data = (data)=>{
-    const cleanedString = input.slice(1, -1)
+    const cleanedString = data.slice(1, -1)
     const jsonString = cleanedString.replace(/(\w+):/g, '"$1":')
     return JSON.parse(jsonString)
 }
