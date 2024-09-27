e7eaac0f3aab7a78ea9dfdc178fa581bec07085c
= <=>
2024-09-27 12:13:51 +0300

    commit avant test encadreur

--- tekeco.js
+++ tekeco.js
@@ -88,6 +88,7 @@ io.on('connection', (socket) => {
         if (topic === 'esp32/pzem') {
             let data = JSON.parse(payload.toString())
             const { power, energy } = data
+            console.log({power,energy})
             setPower({power,energy})
             device.emit('vitesse', 'esp32/pzem', data.power)
             device.emit('consommation', 'esp32/pzem', data.energy)
