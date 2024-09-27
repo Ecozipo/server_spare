86557ae94d8ccc73e75f54a13d056a639f2ee66f
= <=>
2024-09-28 01:45:44 +0300

    fck

--- controllers/ProController.js
+++ controllers/ProController.js
@@ -6,11 +6,29 @@ const prisma = new PrismaClient()
 export const getAllProfessionnel = async (req, res) => {
     try {
 
-        const utilisateurs = prisma.professionnel.findMany({
+        const pros = []
+        //select id,nom,mail,telephone,Quartier.quartier as nom_quartier,url,image from Professionnel
+        
+        const allProfessionnel = await prisma.professionnel.findMany({
             include: {
                 Quartier: true,
                 TypeProfessionnel: true
             }
+        })
+
+        if(!allProfessionnel) return res.status(500).json({ messageError: "Erreur de connexion" })
+            
+        allProfessionnel.forEach(element => {
+            pros.push({
+                id: element.id,
+                nom: element.nom,
+                mail: element.mail,
+                telephone: element.telephone,
+                quartier: element.Quartier.quartier,
+                profession: element.TypeProfessionnel.type,
+                url: element.url,
+                image: element.image
+            })
         })
 
         res.status(200).send(utilisateurs)
