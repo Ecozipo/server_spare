166ff0a2546be6924bc71f74956123681a2b6ce1
Jossuc4 <larryjossuc4@gmail.com>
2024-10-15 17:00:36 +0300

    je sais pas quoi

--- controllers/StatsController.js
+++ controllers/StatsController.js
@@ -1,14 +1,9 @@
 import { PrismaClient } from "@prisma/client"
 import { getPercent } from "../data/State.js"
-import { getHours } from "../data/State.js"
 
 
 const prisma = new PrismaClient()
 
 export const getStats = async (req, res) => {
     res.status(200).json(await getPercent())
-}
-
-export const getHoursStats = async (req, res) => {
-  res.status(200).json(await getHours())
 }
