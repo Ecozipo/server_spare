bdab709bdad152c15a25c785f144623b28cfe76e
Jossuc4 <larryjossuc4@gmail.com>
2024-10-15 23:58:19 +0300

    reparation merge

--- controllers/ConsommationController.js
+++ controllers/ConsommationController.js
@@ -1,27 +1,105 @@
 import { PrismaClient } from "@prisma/client";
 import { format_data } from "../data/functions.js";
+import moment from "moment-timezone";
 
 const prisma = new PrismaClient()
 
-export const journalier = async (req, res) => {
-    const consommations = await prisma.consomation.findMany()
-    res.status(200).json(consommations)
-}
+const getDays = async () => {
 
-export const hebdomadaire = async (req, res) => {
-    console.log("hebdomadaire")
-   const consommations = await prisma.consomation.findMany({
-        orderBy:{
-            date_consommation: 'desc'
-        },
-        take: 7
+    let days = []
+
+    const consommations = await prisma.consomation.findMany(
+        {
+            orderBy: {
+                date_consommation: 'asc'
+            },
+        }
+    )
+
+    days = consommations.filter(day => {
+        
+        const date = new Date(day.date_consommation)
+
+        return date.getHours() === 0 && date.getMinutes() === 0 && date.getSeconds() === 0
+        
     })
 
-    consommations.forEach(element => {
+    days.forEach(element => {
         element.valeur = format_data(element.valeur)
     })
 
-    res.status(200).json(consommations.reverse())
+    return days
+}
+
+export const journalier = async (req, res) => {
+
+    let days = await getDays()
+
+    let consommation = []
+    
+    days.forEach(day => {
+
+        consommation.push(
+            {
+                date: moment(day.date_consommation).locale('fr').format('dddd DD MMMM YYYY'),
+                consommation: day.valeur.energy
+            }
+        )
+
+    })
+    
+    res.status(200).json(consommation)
+}
+
+export const hebdomadaire = async (req, res) => {
+    
+    let days = await getDays()
+
+    let last_week = [
+        moment().tz('Indian/Antananarivo').subtract(1,'week').startOf('week').toISOString(),
+        moment().tz('Indian/Antananarivo').subtract(1,'week').endOf('week').toISOString()
+    ]
+
+    last_week = days.filter(day => {
+        const date = new Date(day.date_consommation)
+        return moment(date).tz('Indian/Antananarivo').isBetween(last_week[0],last_week[1])
+    })
+
+    let this_week = [
+        moment().tz('Indian/Antananarivo').startOf('week').toISOString(),
+        moment().tz('Indian/Antananarivo').endOf('week').toISOString()
+    ]
+
+    this_week = days.filter(day => {
+        const date = new Date(day.date_consommation)
+        return moment(date).tz('Indian/Antananarivo').isBetween(this_week[0],this_week[1])
+    })
+
+    let consommations = {
+        this_week: [],
+        last_week: []
+    }
+
+    last_week.forEach(day => {
+        consommations["last_week"].push(
+            {
+                date: moment(day.date_consommation).locale('fr').format('dddd DD MMMM YYYY'),
+                consommation: day.valeur.energy
+            }
+        )
+    })
+
+    this_week.forEach(day => {
+        consommations["this_week"].push(
+            {
+                date: moment(day.date_consommation).locale('fr').format('dddd DD MMMM YYYY'),
+                consommation: day.valeur.energy
+            }
+        )
+    })
+
+    res.status(200).json(consommations)
+
 }
 
 export const mensuel = (req, res) => {
