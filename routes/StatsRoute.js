c7b934b1b8c4eb6408dd2340c79e9c29ace76604
Jossuc4 <larryjossuc4@gmail.com>
2024-10-16 08:08:51 +0300

    getHours not implemented in routes/StatsRoute.js

--- routes/StatsRoute.js
+++ routes/StatsRoute.js
@@ -1,11 +1,11 @@
 import {Router} from 'express'
-import { getStats,getHoursStats } from '../controllers/StatsController.js'
+import { getStats } from '../controllers/StatsController.js'
 
 
 const router = Router()
 
 router.get('/percent',getStats)
-router.get('/hour',getHoursStats)
+// router.get('/hour',getHoursStats)
 
 
 export default router
