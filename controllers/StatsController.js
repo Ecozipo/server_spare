7e56b04ab9e75d9b4735e02841dfe967ee1d6343
Tiantsoa05 <tiantsoahortencia@gmail.com>
2024-10-17 12:42:05 +0300

    route hour get

--- controllers/StatsController.js
+++ controllers/StatsController.js
@@ -7,3 +7,8 @@ const prisma = new PrismaClient()
 export const getStats = async (req, res) => {
     res.status(200).json(await getPercent())
 }
+
+export const getHoursStats = async (req, res) => {
+    const hours = await prisma.consomation.count()
+    res.status(200).json(hours)
+}
