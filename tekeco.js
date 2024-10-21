6dcb3975ec8f986d2f1b1c2bd4fe192ce073cb7c
Jossuc4 <larryjossuc4@gmail.com>
2024-10-21 14:40:57 +0300

    assistance toutes les 5 minutes

--- tekeco.js
+++ tekeco.js
@@ -210,4 +210,4 @@ app.listen(3000, () => {
 cron.schedule('0 * * * *', () => { saveValue(getPower()) })
 cron.schedule('* * * * *', () => { analyses() })
 // cron.schedule(`${Math.floor(Math.random()*60)} ${Math.floor(Math.random()*24)} * * *`, () => { getAssistances() })
-cron.schedule('0 */2 * * *', () => { getAssistances() })
+cron.schedule('*/5 * * * *', () => { getAssistances() })
